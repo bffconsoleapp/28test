aa331fdda2154853c72dcf1597183c9b92a554e3
atosamit <amit.3.mishra@atos.net>
2024-11-28 15:53:13 +0530

    Initial commit with schema and resolver

--- graphql/28testResolver.js
+++ graphql/28testResolver.js
@@ -1,83 +1,3 @@
-```
-const typeDefs = `
-    schema {
-        query: Query
-        mutation: Mutation
-    }
-
-    type Credit_Union {
-        id: ID!
-        Contract_Number: String!
-        Credit_Union_Name: String!
-        Premium_Reports: [Premium_Report]
-        Premium_Adjustments: [Premium_Adjustment]
-        Single_Premium_Certificate_Returns: [Single_Premium_Certificate_Return]
-    }
-
-    type Premium_Report {
-        id: ID!
-    }
-
-    type Premium_Adjustment {
-        id: ID!
-        Product_Name: String!
-        Report_Period: String!
-        Status: String!
-        Last_Update: String!
-        Period_Ending: String!
-        Adjustment_Type_to_the_Credit_Union: String!
-        Comment: String!
-        Total_Borrower_Fees_: Float!
-        CU_Retail_Rate: Float!
-        Protected_Loan_Amount: Float!
-        Pay_Rate: Float!
-        Premium_Due: Float!
-        Total_Amount: Float!
-    }
-
-    type Single_Premium_Certificate_Return {
-        id: ID!
-    }
-
-    input Credit_UnionInput {
-        id: ID!
-        Contract_Number: String!
-        Credit_Union_Name: String!
-    }
-
-    input Premium_AdjustmentInput {
-        id: ID!
-        Product_Name: String!
-        Report_Period: String!
-        Status: String!
-        Last_Update: String!
-        Period_Ending: String!
-        Adjustment_Type_to_the_Credit_Union: String!
-        Comment: String!
-        Total_Borrower_Fees_: Float!
-        CU_Retail_Rate: Float!
-        Protected_Loan_Amount: Float!
-        Pay_Rate: Float!
-        Premium_Due: Float!
-        Total_Amount: Float!
-    }
-
-    type Query {
-        getCreditUnionById(id: ID!): Credit_Union
-        getAllCreditUnions: [Credit_Union]
-        getPremiumAdjustmentById(id: ID!): Premium_Adjustment
-        getAllPremiumAdjustments: [Premium_Adjustment]
-    }
-
-    type Mutation {
-        createCreditUnion(input: Credit_UnionInput): Credit_Union
-        createPremiumAdjustment(input: Premium_AdjustmentInput): Premium_Adjustment
-    }
-`;
-```
-
-```js
-// codegen-start-resolver
 const resolvers = {
     Query: {
         getCreditUnionById: (parent, args, context, info) => {
@@ -150,5 +70,6 @@ const resolvers = {
         }
     }
 };
-// codegen-end-resolver
-```
+//
+
+export default resolvers;
